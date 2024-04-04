e0f7ca16e91789ea32830915f41e96d118210697
aligomaa56 <3looosh56565656@gmail.com>
2024-04-04 23:49:48 +0200

    task-2/fix-1

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,27 +10,6 @@ env.hosts = ['54.84.254.93', '34.229.71.191']
 env.user = 'ubuntu'
 
 
-def do_pack():
-    '''Packs the contents of the web_static folder into a .tgz archive'''
-    now = datetime.now()
-    datetime_format = '%Y%m%d%H%M%S'
-    formatted_time = now.strftime(datetime_format)
-    archive_path = 'versions/web_static_{}.tgz'.format(formatted_time)
-
-    local('mkdir -p versions')
-    result = local('tar -cvzf {} web_static'.format(
-        archive_path), capture=True)
-
-    if result.failed:
-        print('Failed to pack web_static')
-        return None
-
-    archive_size = os.path.getsize(archive_path)
-    print('web_static packed: {} -> {}Bytes'.format(archive_path,
-                                                    archive_size))
-    return archive_path
-
-
 def do_deploy(archive_path):
     '''Deploy archive to web server'''
     if not archive_path:
