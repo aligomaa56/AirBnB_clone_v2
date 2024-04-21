d97fab1f361f3745e489b2eae727d0462c428950
aligomaa56 <3looosh56565656@gmail.com>
2024-04-21 21:44:19 +0200

    task-5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """script that starts a Flask web application"""
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -38,6 +38,12 @@ def python_text(text):
 def n_number(n):
     """ Returns HBNB """
     return '{} is a number'.format(str(n))
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """ Returns HBNB """
+    return render_template('5-number.html', n=str(n))
 
 
 if __name__ == '__main__':
